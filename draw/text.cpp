b88921e363d4108390e786f0af261dabce9e1990
kostin_v <kostin_v@cdcom.ru>
2021-01-18 17:16:58 +0300

    загрузка символов (пока неясно откуда)

--- draw/text.cpp
+++ draw/text.cpp
@@ -1,12 +1,65 @@
 #include "../def.h"
 #include "text.h"
 #include "ft2build.h"
+#include "opengl.h"
 #include FT_FREETYPE_H
 
 TextDrawing::TextDrawing()
 {
-	FT_Library ft;
-	check(FT_Init_FreeType(&ft) == 0);
+	check(FT_Init_FreeType(&ft_) == 0);
+
+    check(FT_New_Face(ft_, "fonts/arial.ttf", 0, &face_) == 0);
+
+    fill_characters();
+}
+
+TextDrawing::~TextDrawing()
+{
+    FT_Done_Face(face_);
+    FT_Done_FreeType(ft_);
+}
+
+void TextDrawing::fill_characters()
+{
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // отключаем ограничение выравнивания байтов
+
+    for (unsigned int c = 0; c < 256; c++)
+    {
+        // Загружаем глифы символов 
+        check(FT_Load_Char(face_, c, FT_LOAD_RENDER) == 0);
+
+        // Генерируем текстуру
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face_->glyph->bitmap.width,
+            face_->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face_->glyph->bitmap.buffer
+        );
+
+        // Задаем для текстуры необходимые опции
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // Сохраняем символ для последующего использования
+        Character character = 
+        {
+            texture,
+            face_->glyph->bitmap.width, face_->glyph->bitmap.rows,
+            face_->glyph->bitmap_left, face_->glyph->bitmap_top,
+            face_->glyph->advance.x
+        };
+        Characters.insert({ c, character });
+    }
 }
 
 TextDrawing& text_drawing()
--- draw/text.h
+++ draw/text.h
@@ -1,9 +1,33 @@
 #pragma once
+#include<map>
+#include "ft2build.h"
+#include FT_FREETYPE_H
 
 class TextDrawing
 {
+    FT_Library ft_;
+    FT_Face face_;
+
+    struct Character
+    {
+        unsigned int TextureID; // ID текстуры глифа
+
+        // размер глифа
+        unsigned int width_;
+        unsigned int height_;
+
+        // смещение от линии шрифта до верхнего/левого угла глифа
+        int left_;
+        int top_;
+
+        FT_Pos advance_; // смещение до следующего глифа
+    };
+
+    std::map<char, Character> Characters;
+    void fill_characters();
 public:
 	TextDrawing();
+    ~TextDrawing();
 };
 
 TextDrawing& text_drawing();
